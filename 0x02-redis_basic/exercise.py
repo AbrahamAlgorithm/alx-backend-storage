260a51b952df796e4e56cbd8c347d6b95ecc3902
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-20 00:09:48 +0100

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,19 +1,50 @@
 #!/usr/bin/env python3
 """
-Cache module for storing data in Redis.
+Cache module for storing and retrieving data in Redis.
 """
 
 import redis
 import uuid
 from typing import Union, Callable, Optional
+import functools
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts the number of times a method is called.
+
+    Args:
+        method (Callable): The method to be wrapped by the decorator.
+
+    Returns:
+        Callable: The wrapped method with counting functionality.
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function to count the number of calls to the method.
+
+        Args:
+            self: The instance of the class.
+            *args: The positional arguments of the method.
+            **kwargs: The keyword arguments of the method.
+
+        Returns:
+            The return value of the original method.
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
     """
-    Cache class for storing data in Redis.
+    Cache class for storing and retrieving data in Redis.
 
     The Cache class provides methods to store data in a Redis database
-    using a randomly generated key.
+    using a randomly generated key, and retrieve data with optional
+    conversion functions.
     """
 
     def __init__(self):
@@ -26,6 +57,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in Redis using a randomly generated key.
@@ -39,7 +71,6 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-
 
     def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
         """
@@ -89,12 +120,10 @@ class Cache:
 if __name__ == "__main__":
     cache = Cache()
 
-    TEST_CASES = {
-        b"foo": None,
-        123: int,
-        "bar": lambda d: d.decode("utf-8")
-    }
+    cache.store(b"first")
+    print(cache.get(cache.store.__qualname__))
 
-    for value, fn in TEST_CASES.items():
-        key = cache.store(value)
-        assert cache.get(key, fn=fn) == value
+    cache.store(b"second")
+    cache.store(b"third")
+    print(cache.get(cache.store.__qualname__))
+
