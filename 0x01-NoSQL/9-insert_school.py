2c1112b5922a67c1ceeedcaffb0d336892436dd4
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-20 04:59:40 +0100

    Insert a document in Python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Insert a document in Python
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Insert a document in Python'''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
